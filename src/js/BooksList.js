212639ea540ea1a816809f5ce239450ba1e43ddd
Błażej Potasiński <blazej.potasinski@wp.pl>
2026-01-11 15:51:50 +0100

    9.5

---
+++ src/js/BooksList.js
@@ -0,0 +1,82 @@
+/* global Handlebars, dataSource */
+
+import { utils } from './functions.js';
+
+export class BooksList {
+  constructor() {
+    this.data = dataSource.books;
+    this.filters = [];
+    this.favoriteBooks = [];
+
+    this.getElements();
+    this.render();
+    this.initActions();
+  }
+
+  getElements() {
+    this.dom = {};
+    this.dom.booksList = document.querySelector('.books-list');
+    this.dom.filters = document.querySelectorAll('.filters input');
+  }
+
+  render() {
+    for (const book of this.data) {
+      const generatedHTML = Handlebars.compile(
+        document.querySelector('#template-book').innerHTML
+      )(book);
+
+      const element = utils.createDOMFromHTML(generatedHTML);
+      this.dom.booksList.appendChild(element);
+    }
+  }
+
+  initActions() {
+    for (const checkbox of this.dom.filters) {
+      checkbox.addEventListener('change', event => {
+        const value = event.target.value;
+
+        if (event.target.checked) {
+          this.filters.push(value);
+        } else {
+          const index = this.filters.indexOf(value);
+          this.filters.splice(index, 1);
+        }
+
+        this.filterBooks();
+      });
+    }
+
+    this.dom.booksList.addEventListener('dblclick', event => {
+      event.preventDefault();
+
+      const bookImage = event.target.closest('.book__image');
+      if (!bookImage) return;
+
+      const bookId = bookImage.dataset.id;
+
+      bookImage.classList.toggle(
+        'favorite',
+        !bookImage.classList.contains('favorite')
+      );
+    });
+  }
+
+  filterBooks() {
+    for (const book of this.data) {
+      let shouldBeHidden = false;
+
+      for (const filter of this.filters) {
+        if (!book.details[filter]) {
+          shouldBeHidden = true;
+          break;
+        }
+      }
+
+      const bookImage = document.querySelector(
+        `.book__image[data-id="${book.id}"]`
+      );
+
+      bookImage.classList.toggle('hidden', shouldBeHidden);
+    }
+  }
+}
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,11 +1,7 @@
-/* global document */
-
-'use strict';
-
-const utils = {};
-
-utils.createDOMFromHTML = function(htmlString) {
-  const div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
+export const utils = {
+  createDOMFromHTML: function(htmlString) {
+    const div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
+  },
 };
